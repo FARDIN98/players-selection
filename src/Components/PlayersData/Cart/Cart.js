f23a30897bf941b8e1d38ef28ad6fd12d056371a
Fardin Ahmed <fardinahmed66@gmail.com>
2021-03-01 21:06:02 +0600

    first commit

---
+++ src/Components/PlayersData/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = (team) => {
+    // const cricketer = props
+    console.log(team)
+    return (
+        <div>
+            {/* <h1>This is cart: {props.team}</h1> */}
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/Components/PlayersData/PlayersData.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './PlayersData.css'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+const PlayersData = (props) => {
+    console.log(props);
+    const {image,name,salary} = props.player
+    const setSize = {
+        height:"400px"
+    }
+    const handleAddPlayer = props.handleAddPlayer
+    return (
+        <div className="card">
+            <img style={setSize} src={image} alt=""/>
+            <h3>Name: {name}</h3>
+            <h4>Salary: {salary}</h4>
+            <button onClick={()=>handleAddPlayer()} className="btn btn-primary"><FontAwesomeIcon icon={faUser} /> Pick Player</button>
+        </div>
+    );
+};
+
+export default PlayersData;
